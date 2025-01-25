a6b587d75d81b82dc3290f0a4405d56ff8b57120
Byeori Kim <bk.byeori.kim@gmail.com>
2025-01-25 00:21:14 -0800

    router minor change

--- src/appController.js
+++ src/appController.js
@@ -3,9 +3,7 @@ const appService = require('./appService');
 
 const router = express.Router();
 
-// ----------------------------------------------------------
-// API endpoints
-// Modify or extend these routes based on your project's needs.
+
 router.get('/check-db-connection', async (req, res) => {
     const isConnect = await appService.testOracleConnection();
     if (isConnect) {
